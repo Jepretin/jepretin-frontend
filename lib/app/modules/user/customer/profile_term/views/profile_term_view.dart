c8a4402d534860d852ab37a99d3ff11248d8973b
azrmhenn <azrmhenn@gmail.com>
2025-09-19 23:23:08 +0700

    add page profile term

--- lib/app/modules/user/customer/profile_term/views/profile_term_view.dart
+++ lib/app/modules/user/customer/profile_term/views/profile_term_view.dart
@@ -3,22 +3,51 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/profile_term_controller.dart';
+import 'package:jepretin/app/shared/customCardContent.dart';
+import 'package:jepretin/app/shared/customComponent.dart';
 
 class ProfileTermView extends GetView<ProfileTermController> {
   const ProfileTermView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProfileTermView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ProfileTermView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('Syarat dan ketentuan'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: GetBuilder(
+            init: ProfileTermController(),
+            builder: (controller) => SingleChildScrollView(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                  child: Column(
+                    children: [
+                      InputWithLabel(
+                          label: 'Syarat',
+                          input: CustomCardSingle(
+                              child: Row(
+                            children: [
+                              Text(
+                                "Syarat\n\n"
+                                "1. Syarat penggunaan.\n"
+                                "2. Syarat penggunaan.\n"
+                                ,
+                              )
+                            ],
+                          ))),
+                      InputWithLabel(
+                          label: 'Ketentuan',
+                          input: CustomCardSingle(
+                              child: Row(
+                            children: [
+                              Text(
+                                "Ketentuan\n\n"
+                                "1. Ketentuan penggunaan.\n"
+                                "2. Ketentuan penggunaan.\n"
+                              )
+                            ],
+                          ))),
+                    ],
+                  ),
+                )));
   }
 }
