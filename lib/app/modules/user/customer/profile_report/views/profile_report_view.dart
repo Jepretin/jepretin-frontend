b5a7626a897f589bb70713c10f7219225067a315
azrmhenn <azrmhenn@gmail.com>
2025-09-19 23:43:00 +0700

    add page profile report

--- lib/app/modules/user/customer/profile_report/views/profile_report_view.dart
+++ lib/app/modules/user/customer/profile_report/views/profile_report_view.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:jepretin/app/themes/themes.dart';
 
 import '../controllers/profile_report_controller.dart';
+import 'package:jepretin/app/shared/customCardContent.dart';
+import 'package:jepretin/app/shared/customComponent.dart';
 
 class ProfileReportView extends GetView<ProfileReportController> {
   const ProfileReportView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProfileReportView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ProfileReportView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('Laporkan'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: GetBuilder(
+            init: ProfileReportController(),
+            builder: (controller) => SingleChildScrollView(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                  child: Column(
+                    children: [
+                      InputWithLabel(
+                          label: 'Laporkan Masalah',
+                          input: CustomCardSingle(
+                              child: Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: 'Ketik penjelasan.....',
+                                      border: InputBorder.none),
+                                ),
+                              )
+                            ],
+                          ))),
+                      SizedBox(height: 20),
+                      Container(
+                        alignment: Alignment.center,
+                        width: 162,
+                        height: 127,
+                        decoration: BoxDecoration(
+                            color: whiteColor,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 35,
+                                  color: blackColor.withOpacity(0.25),
+                                  spreadRadius: -5)
+                            ]),
+                        child: Icon(Icons.upload),
+                      )
+                    ],
+                  ),
+                )));
   }
 }
